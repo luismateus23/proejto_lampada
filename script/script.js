2fb65ddae8747eda42b97eeb50e0cf860296cd61
Luís Mateus Billar Antonietti <luismateusbillar@gmail.com>
2022-04-07 10:13:37 -0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,21 @@
+
+function entrar(){
+   let imagem = document.getElementById('imagem');
+   imagem.src = '../img/ligada.jpg';
+}
+function sair(){
+    let imagem = document.getElementById('imagem');
+    imagem.src = '../img/desligada.jpg';
+}
+function ligar(){
+    let imagem = document.getElementById('imagem');
+    imagem.src = '../img/ligada.jpg';
+}
+function desligar(){
+    let imagem = document.getElementById('imagem');
+    imagem.src = '../img/desligada.jpg';
+}
+function quebrar(){
+    let imagem = document.getElementById('imagem');
+    imagem.src = '../img/quebrada.jpg';
+}
